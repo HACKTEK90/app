49256968236f37309bb2219087cff5e180298af3
RuokGamerz <79328354+HACKTEK90@users.noreply.github.com>
2025-05-27 23:33:59 +0530

    Update src.java

--- src.java
+++ src.java
@@ -1,1 +1,15 @@
+ function launchFullscreen() {
+      const elem = document.documentElement;
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+      }
+    }
 
+    // Trigger fullscreen on first tap/click anywhere
+    document.addEventListener("click", function () {
+      launchFullscreen();
+    }, { once: true }); // Only trigger once
